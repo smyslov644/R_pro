ef63583149d605b69dbde1d74874c97c72862137
ZERO <rpku326159487@gmail.com>
2019-03-28 00:59:03 +0900

    Initial commit

---
+++ R연습.R
@@ -0,0 +1,18 @@
+
+# data = read.table(file.choose(),header(TRUE))
+# head(lungdata)
+# tail(lungdata)
+# 
+# data_2= read.csv(file.choose(),header(TRUE))
+# head(data_2)
+# tail(data_2)
+# str(data_2)
+# max(data_2$Gender)
+
+# num_of_rolls = 100000
+# dice_1 = as.integer(runif(num_of_rolls,min=1,max=7))
+# dice_2 = as.integer(runif(num_of_rolls,min=1,max=7))
+# dice_3 = as.integer(runif(num_of_rolls,min=1,max=7))
+# data = table(dice_1 + dice_2 + dice_3)
+
+
